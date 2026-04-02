be4b28cd72eca617c505c3bf36e0fb6fd11568d0
imamenemame <kristianbaychev88@gmail.com>
2026-04-02 14:06:37 +0300

    Created Login and dashboard

---
+++ SchoolLabs/SchoolLabs.UI/Dashboard.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace SchoolLabs.UI
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            dataGridView1 = new DataGridView();
+            Asetname = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 21F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(242, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(131, 33);
+            label1.TabIndex = 0;
+            label1.Text = "Welcome, ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(423, 61);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 15);
+            label2.TabIndex = 1;
+            label2.Text = "username";
+            label2.Click += this.label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 16F);
+            label3.Location = new Point(277, 105);
+            label3.Name = "label3";
+            label3.Size = new Size(197, 30);
+            label3.TabIndex = 2;
+            label3.Text = "Role: Administrator";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(115, 512);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 3;
+            label4.Text = "label4";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(115, 176);
+            button1.Name = "button1";
+            button1.Size = new Size(105, 38);
+            button1.TabIndex = 4;
+            button1.Text = "Assets";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(267, 176);
+            button2.Name = "button2";
+            button2.Size = new Size(106, 38);
+            button2.TabIndex = 5;
+            button2.Text = "Loans";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(404, 176);
+            button3.Name = "button3";
+            button3.Size = new Size(102, 38);
+            button3.TabIndex = 6;
+            button3.Text = "Reports";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(563, 176);
+            button4.Name = "button4";
+            button4.Size = new Size(101, 38);
+            button4.TabIndex = 7;
+            button4.Text = "Users";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += this.button4_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Asetname });
+            dataGridView1.Location = new Point(115, 240);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(549, 239);
+            dataGridView1.TabIndex = 8;
+            // 
+            // Asetname
+            // 
+            Asetname.HeaderText = "Column1";
+            Asetname.Name = "Asetname";
+            // 
+            // Dashboard
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(753, 628);
+            Controls.Add(dataGridView1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Dashboard";
+            Text = "Form1";
+            Load += this.Dashboard_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Asetname;
+    }
+}
---
+++ SchoolLabs/SchoolLabs.UI/LoginPage.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace SchoolLabs.UI
+{
+    partial class LoginPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            button1 = new Button();
+            fileSystemWatcher1 = new FileSystemWatcher();
+            panel1 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)fileSystemWatcher1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.Location = new Point(32, 186);
+            label1.Name = "label1";
+            label1.Size = new Size(102, 22);
+            label1.TabIndex = 0;
+            label1.Text = "Username :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label2.Location = new Point(32, 240);
+            label2.Name = "label2";
+            label2.Size = new Size(100, 22);
+            label2.TabIndex = 1;
+            label2.Text = "Password :";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(154, 185);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(157, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(154, 242);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(157, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Wide Latin", 21F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label3.Location = new Point(32, 13);
+            label3.Name = "label3";
+            label3.Size = new Size(279, 34);
+            label3.TabIndex = 5;
+            label3.Text = "SchoolLab";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label4.Location = new Point(32, 91);
+            label4.Name = "label4";
+            label4.Size = new Size(67, 24);
+            label4.TabIndex = 6;
+            label4.Text = "Login ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Times New Roman", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label5.Location = new Point(32, 128);
+            label5.Name = "label5";
+            label5.Size = new Size(136, 17);
+            label5.TabIndex = 7;
+            label5.Text = "Log into your account";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button1.Location = new Point(118, 304);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 33);
+            button1.TabIndex = 8;
+            button1.Text = "Continue";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // fileSystemWatcher1
+            // 
+            fileSystemWatcher1.EnableRaisingEvents = true;
+            fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Azure;
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(label2);
+            panel1.Location = new Point(157, 70);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(344, 369);
+            panel1.TabIndex = 9;
+            // 
+            // LoginPage
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightCyan;
+            ClientSize = new Size(663, 522);
+            Controls.Add(panel1);
+            Name = "LoginPage";
+            Text = "LoginPage";
+            ((System.ComponentModel.ISupportInitialize)fileSystemWatcher1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button1;
+        private FileSystemWatcher fileSystemWatcher1;
+        private Panel panel1;
+    }
+}
