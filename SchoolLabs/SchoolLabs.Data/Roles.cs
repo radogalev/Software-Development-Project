2e2328edf4ba12fd9c790ee955bda6b9f4f81510
MarioYovchev <mario.yovchev@gmail.com>
2026-04-23 13:24:01 +0300

    Added a new class

---
+++ SchoolLabs/SchoolLabs.Data/Roles.cs
@@ -0,0 +1,13 @@
+﻿namespace SchoolLabs.Data
+{
+    public class Roles
+    {
+       public int RoledId {  get; set; }
+        public enum RoleName { 
+            Administrator,
+            Lab_Assistant,
+            Viewer
+        }
+        public ICollection<User> Users { get; set; }
+    }
+}
